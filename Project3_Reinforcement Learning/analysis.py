a35eced0099519d303f181804a9d88c612948e44
Mahdi-Rahmani <mah.rah79@yahoo.com>
2023-02-22 21:07:34 +0330

    question3 is finished

--- Project3_Reinforcement Learning/analysis.py
+++ Project3_Reinforcement Learning/analysis.py
@@ -53,9 +53,9 @@ def question3d():
     # If not possible, return 'NOT POSSIBLE'
 
 def question3e():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    answerDiscount = 0.9
+    answerNoise = 0.2
+    answerLivingReward = 10
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
